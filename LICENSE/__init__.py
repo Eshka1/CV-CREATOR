d6cf5dd48fcad3577cf4275c54dba4f7bc0cd177
IFREET NAWAR <ifreetnawar@gmail.com>
2026-07-24 04:22:10 +0600

    Add files via upload

--- LICENSE/__init__.py
+++ LICENSE/__init__.py
@@ -1,34 +1,13 @@
-# Copyright 2016 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+from __future__ import annotations
 
-"""Google Auth Library for Python."""
-
-import logging
-
-from google.auth import version as google_auth_version
-from google.auth._default import (
-    default,
-    load_credentials_from_dict,
-    load_credentials_from_file,
-)
+__version__ = "26.1.2"
 
 
-__version__ = google_auth_version.__version__
+def main(args: list[str] | None = None) -> int:
+    """This is an internal API only meant for use by pip's own console scripts.
 
+    For additional details, see https://github.com/pypa/pip/issues/7498.
+    """
+    from pip._internal.utils.entrypoints import _wrapper
 
-__all__ = ["default", "load_credentials_from_file", "load_credentials_from_dict"]
-
-
-# Set default logging handler to avoid "No handler found" warnings.
-logging.getLogger(__name__).addHandler(logging.NullHandler())
+    return _wrapper(args)
