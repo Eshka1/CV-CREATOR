63710bd1b4321f286d1a7335d45cfc1dbc2b92bd
IFREET NAWAR <ifreetnawar@gmail.com>
2026-07-24 04:29:01 +0600

    Add files via upload

--- LICENSE/exceptions.py
+++ LICENSE/exceptions.py
@@ -1,108 +1,152 @@
-# Copyright 2016 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+"""
+requests.exceptions
+~~~~~~~~~~~~~~~~~~~
 
-"""Exceptions used in the google.auth package."""
+This module contains the set of Requests' exceptions.
+"""
+
+from pip._vendor.urllib3.exceptions import HTTPError as BaseHTTPError
+
+from .compat import JSONDecodeError as CompatJSONDecodeError
 
 
-class GoogleAuthError(Exception):
-    """Base class for all google.auth errors."""
+class RequestException(IOError):
+    """There was an ambiguous exception that occurred while handling your
+    request.
+    """
 
     def __init__(self, *args, **kwargs):
-        super(GoogleAuthError, self).__init__(*args)
-        retryable = kwargs.get("retryable", False)
-        self._retryable = retryable
-
-    @property
-    def retryable(self):
-        return self._retryable
-
-
-class TransportError(GoogleAuthError):
-    """Used to indicate an error occurred during an HTTP request."""
+        """Initialize RequestException with `request` and `response` objects."""
+        response = kwargs.pop("response", None)
+        self.response = response
+        self.request = kwargs.pop("request", None)
+        if response is not None and not self.request and hasattr(response, "request"):
+            self.request = self.response.request
+        super().__init__(*args, **kwargs)
 
 
-class RefreshError(GoogleAuthError):
-    """Used to indicate that an refreshing the credentials' access token
-    failed."""
+class InvalidJSONError(RequestException):
+    """A JSON error occurred."""
 
 
-class UserAccessTokenError(GoogleAuthError):
-    """Used to indicate ``gcloud auth print-access-token`` command failed."""
+class JSONDecodeError(InvalidJSONError, CompatJSONDecodeError):
+    """Couldn't decode the text into json"""
+
+    def __init__(self, *args, **kwargs):
+        """
+        Construct the JSONDecodeError instance first with all
+        args. Then use it's args to construct the IOError so that
+        the json specific args aren't used as IOError specific args
+        and the error message from JSONDecodeError is preserved.
+        """
+        CompatJSONDecodeError.__init__(self, *args)
+        InvalidJSONError.__init__(self, *self.args, **kwargs)
+
+    def __reduce__(self):
+        """
+        The __reduce__ method called when pickling the object must
+        be the one from the JSONDecodeError (be it json/simplejson)
+        as it expects all the arguments for instantiation, not just
+        one like the IOError, and the MRO would by default call the
+        __reduce__ method from the IOError due to the inheritance order.
+        """
+        return CompatJSONDecodeError.__reduce__(self)
 
 
-class DefaultCredentialsError(GoogleAuthError):
-    """Used to indicate that acquiring default credentials failed."""
+class HTTPError(RequestException):
+    """An HTTP error occurred."""
 
 
-class MutualTLSChannelError(GoogleAuthError):
-    """Used to indicate that mutual TLS channel creation is failed, or mutual
-    TLS channel credentials is missing or invalid."""
+class ConnectionError(RequestException):
+    """A Connection error occurred."""
 
 
-class ClientCertError(GoogleAuthError):
-    """Used to indicate that client certificate is missing or invalid."""
-
-    @property
-    def retryable(self):
-        return False
+class ProxyError(ConnectionError):
+    """A proxy error occurred."""
 
 
-class OAuthError(GoogleAuthError):
-    """Used to indicate an error occurred during an OAuth related HTTP
-    request."""
+class SSLError(ConnectionError):
+    """An SSL error occurred."""
 
 
-class ReauthFailError(RefreshError):
-    """An exception for when reauth failed."""
+class Timeout(RequestException):
+    """The request timed out.
 
-    def __init__(self, message=None, **kwargs):
-        super(ReauthFailError, self).__init__(
-            "Reauthentication failed. {0}".format(message), **kwargs
-        )
-
-
-class ReauthSamlChallengeFailError(ReauthFailError):
-    """An exception for SAML reauth challenge failures."""
+    Catching this error will catch both
+    :exc:`~requests.exceptions.ConnectTimeout` and
+    :exc:`~requests.exceptions.ReadTimeout` errors.
+    """
 
 
-class MalformedError(DefaultCredentialsError, ValueError):
-    """An exception for malformed data."""
+class ConnectTimeout(ConnectionError, Timeout):
+    """The request timed out while trying to connect to the remote server.
+
+    Requests that produced this error are safe to retry.
+    """
 
 
-class InvalidResource(DefaultCredentialsError, ValueError):
-    """An exception for URL error."""
+class ReadTimeout(Timeout):
+    """The server did not send any data in the allotted amount of time."""
 
 
-class InvalidOperation(DefaultCredentialsError, ValueError):
-    """An exception for invalid operation."""
+class URLRequired(RequestException):
+    """A valid URL is required to make a request."""
 
 
-class InvalidValue(DefaultCredentialsError, ValueError):
-    """Used to wrap general ValueError of python."""
+class TooManyRedirects(RequestException):
+    """Too many redirects."""
 
 
-class InvalidType(DefaultCredentialsError, TypeError):
-    """Used to wrap general TypeError of python."""
+class MissingSchema(RequestException, ValueError):
+    """The URL scheme (e.g. http or https) is missing."""
 
 
-class OSError(DefaultCredentialsError, EnvironmentError):
-    """Used to wrap EnvironmentError(OSError after python3.3)."""
+class InvalidSchema(RequestException, ValueError):
+    """The URL scheme provided is either invalid or unsupported."""
 
 
-class TimeoutError(GoogleAuthError):
-    """Used to indicate a timeout error occurred during an HTTP request."""
+class InvalidURL(RequestException, ValueError):
+    """The URL provided was somehow invalid."""
 
 
-class ResponseError(GoogleAuthError):
-    """Used to indicate an error occurred when reading an HTTP response."""
+class InvalidHeader(RequestException, ValueError):
+    """The header value provided was somehow invalid."""
+
+
+class InvalidProxyURL(InvalidURL):
+    """The proxy URL provided is invalid."""
+
+
+class ChunkedEncodingError(RequestException):
+    """The server declared chunked encoding but sent an invalid chunk."""
+
+
+class ContentDecodingError(RequestException, BaseHTTPError):
+    """Failed to decode response content."""
+
+
+class StreamConsumedError(RequestException, TypeError):
+    """The content for this response was already consumed."""
+
+
+class RetryError(RequestException):
+    """Custom retries logic failed"""
+
+
+class UnrewindableBodyError(RequestException):
+    """Requests encountered an error when trying to rewind a body."""
+
+
+# Warnings
+
+
+class RequestsWarning(Warning):
+    """Base warning for Requests."""
+
+
+class FileModeWarning(RequestsWarning, DeprecationWarning):
+    """A file was opened in text mode, but Requests determined its binary length."""
+
+
+class RequestsDependencyWarning(RequestsWarning):
+    """An imported dependency doesn't match the expected version range."""
