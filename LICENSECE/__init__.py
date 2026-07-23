12e063825b484a89ae70446210e097a47da2599b
Nowrin-Tanhiad <nowrintanhiad717@gmail.com>
2026-07-24 03:24:22 +0600

    Add files via upload

--- LICENSECE/__init__.py
+++ LICENSECE/__init__.py
@@ -1,4 +1,13 @@
-from .serving import run_simple as run_simple
-from .test import Client as Client
-from .wrappers import Request as Request
-from .wrappers import Response as Response
+from __future__ import annotations
+
+__version__ = "26.1.2"
+
+
+def main(args: list[str] | None = None) -> int:
+    """This is an internal API only meant for use by pip's own console scripts.
+
+    For additional details, see https://github.com/pypa/pip/issues/7498.
+    """
+    from pip._internal.utils.entrypoints import _wrapper
+
+    return _wrapper(args)
